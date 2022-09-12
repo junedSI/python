5685d68cae75107a9caea1538f6442d9ab54e626
juned <1132210708@mitwpu.edu.in>
2022-09-12 18:32:22 +0530

    variables and data types
    creating variables and assigning values to them. day one of brushing up my python skills.

---
+++ Data_types_and_variables.py
@@ -0,0 +1,40 @@
+print('hello world')
+
+# int data types 
+a = 10 
+print(a)
+print(type(a))
+
+# Integer
+b = 9223372036854775807
+print(b)
+print(type(b))
+
+# Floating point
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# String
+c = 'A'
+print(c)
+print(type(c))
+
+# String
+name = 'John Doe'
+print(name)
+print(type(name))
+
+# Boolean
+q = True
+print(q)
+print(type(q))
+
+# Empty value or null data type
+x = None
+print(x)
+print(type(x))
+
+# Reference
+a, b, c = 1, 2, 3
+print(a, b, c)
