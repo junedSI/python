21efbc86e60859d9cfc65aa98837bdd6152e6f57
juned <1132210708@mitwpu.edu.in>
2023-06-13 11:20:12 +0530

    ;

--- pyqt/demo.py
+++ pyqt/demo.py
@@ -5,38 +5,38 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 
 
-# class LoginWindow(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.setWindowTitle('User Login')
-#         self.setWindowIcon(QIcon("Icon.png"))
-#         self.setGeometry(1200, 300, 700, 700)
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('User Login')
+        self.setWindowIcon(QIcon("Icon.png"))
+        self.setGeometry(1200, 300, 700, 700)
 
-#         # Create labels, line edits, and buttons
-#         self.username_label = QLabel('Username:')
-#         self.username_edit = QLineEdit()
-#         self.password_label = QLabel('Password:')
-#         self.password_edit = QLineEdit()
-#         self.password_edit.setEchoMode(QLineEdit.Password)
-#         self.login_button = QPushButton('Login')
+        # Create labels, line edits, and buttons
+        self.username_label = QLabel('Username:')
+        self.username_edit = QLineEdit()
+        self.password_label = QLabel('Password:')
+        self.password_edit = QLineEdit()
+        self.password_edit.setEchoMode(QLineEdit.Password)
+        self.login_button = QPushButton('Login')
 
-#         # Apply style sheet to customize the look and feel
-#         def set_default_style(self):
-#             # Load the external style sheet from file
-#             with open('login_style.qss', 'r') as file:
-#                 style_sheet = file.read()
+        # Apply style sheet to customize the look and feel
+        def set_default_style(self):
+            # Load the external style sheet from file
+            with open('login_style.qss', 'r') as file:
+                style_sheet = file.read()
 
-#             # Apply the style sheet
-#             self.setStyleSheet(style_sheet)
+            # Apply the style sheet
+            self.setStyleSheet(style_sheet)
 
-#         # Create a vertical layout and add widgets with spacing
-#         layout = QVBoxLayout()
-#         layout.addWidget(self.username_label, alignment=Qt.AlignCenter)
-#         layout.addWidget(self.username_edit)
-#         layout.addWidget(self.password_label, alignment=Qt.AlignCenter)
-#         layout.addWidget(self.password_edit)
-#         layout.addSpacing(20)
-#         layout.addWidget(self.login_button, alignment=Qt.AlignCenter)
+        # Create a vertical layout and add widgets with spacing
+        layout = QVBoxLayout()
+        layout.addWidget(self.username_label, alignment=Qt.AlignCenter)
+        layout.addWidget(self.username_edit)
+        layout.addWidget(self.password_label, alignment=Qt.AlignCenter)
+        layout.addWidget(self.password_edit)
+        layout.addSpacing(20)
+        layout.addWidget(self.login_button, alignment=Qt.AlignCenter)
 
 #         # Set the layout and adjust spacing
 #         container = QWidget()
