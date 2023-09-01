11bbca690574fa55f127e5b519ce12b7ac415bfc
juned inamdar <95338897+junedSI@users.noreply.github.com>
2023-08-31 22:51:07 -0700

    ;

---
+++ CDC_version0.py
@@ -0,0 +1,11 @@
+""" comments """
+
+1.how to impliment CDC using python you have to create an another python script 
+2.to take Excel as inpiut and you have to parser convert it into key value pairs 
+all this task should have to be in 
+{
+    abc:"axaya"
+    
+    jdbacb:"jhbsdckjbs"
+
+}
